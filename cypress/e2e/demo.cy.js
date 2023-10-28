b4033614828405e493376dc7371abfd22fa7a559
IMMORTALWIN\singh <vaibhavei2051@gmail.com>
2023-10-28 19:24:57 +1100

    updated components

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,19 +1,18 @@
 /// <reference types="cypress" />
-import Components from '../Components/Components'
+import Components from '../components/Components'
 import globalData from '../fixtures/global.json'
 import testData from '../fixtures/test.json'
 
 //Test cases
 describe('Automation Practice Tests', () => {
+  const cp = new Components()
   it('Test Case 1: Register User', () => {
-    const cp = new Components()
     cp
       .com_openurl(globalData.url)
       .com_registraion(testData)
       .com_delete_account();
   })
   it('Test Case 2: Login User with correct email and password', () => {
-    const cp = new Components()
     cp
       .com_openurl(globalData.url)
       .com_registraion(testData)
@@ -22,28 +21,24 @@ describe('Automation Practice Tests', () => {
       .com_delete_account();
   })
   it('Test Case 8: Verify All Products and product detail page', () => {
-    const cp = new Components()
     cp
       .com_openurl(globalData.url)
       .com_verify_products_page()
       .com_validate_first_item_product_detail_page()
   })
   it('Test Case 9: Search Product', () => {
-    const cp = new Components()
     cp
       .com_openurl(globalData.url)
       .com_verify_products_page()
       .com_searchAndVerifyProductList(testData.searchItem)
   })
   it('Test Case 12: Add Products in Cart', () => {
-    const cp = new Components()
     cp
       .com_openurl(globalData.url)
       .com_verify_products_page()
       .com_add_to_cart_product_page()
   })
   it('Test Case 13: Verify Product quantity in Cart', () => {
-    const cp = new Components()
     cp
       .com_openurl(globalData.url)
       .com_verify_products_page()
